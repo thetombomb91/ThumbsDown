92faef412ab8b46177501b24e8f007848ce2a8f7
Thomas Desmond <thomasdes533@gmail.com>
2021-01-15 08:05:15 -0800

    Lambda now returns license plate object back with current thumbs down count.

--- lambdas/addOrIncrementItem.js
+++ lambdas/addOrIncrementItem.js
@@ -1,55 +1,60 @@
 const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
 
-const ThumbsDownTableName = 'ThumbsDown';
-
 exports.handler = (event, context, callback) => {
-    var getItemParams = {
-        TableName: ThumbsDownTableName,
+    var getParams = {
+        TableName: 'ThumbsDown',
         Key: {
             'licensePlate': { S: event.licensePlate },
             'state': { S: event.state }
         },
     };
-    dynamodb.getItem(getItemParams, function (err, data) {
+    dynamodb.getItem(getParams, function (err, data) {
         if (err) {
             console.log("Error on GetItem from DB", err);
         } else {
-            console.log("Found existing license plate in DB", data.Item);
+            console.log("Success", data.Item);
 
             if (data.Item === undefined) {
                 addNewLicensePlateToDatabase(event, callback);
 
             } else {
-                IncrementThumbsDownCountByOne(event, data);
+                IncrementThumbsDownCountByOne(event, data.Item['thumbsDownCount']['N'], callback);
             }
         }
     });
 };
 
-function IncrementThumbsDownCountByOne(event, data) {
+function IncrementThumbsDownCountByOne(event, thumbsDownCount, callback) {
     var params = {
-        TableName: ThumbsDownTableName,
+        TableName: 'ThumbsDown',
         Key: {
             'licensePlate': { S: event.licensePlate },
             'state': { S: event.state }
         },
-        ExpressionAttributeValues: { ":inc": {N: "1"} },
+        ExpressionAttributeValues: { ":inc": { N: "1" } },
         UpdateExpression: "ADD thumbsDownCount :inc"
     };
 
+    console.log("Updating the item...", thumbsDownCount);
     dynamodb.updateItem(params, function (err, data) {
         if (err) {
             console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
         } else {
-            console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+            console.log("UpdateItem succeeded:", JSON.stringify(thumbsDownCount, null, 2));
+            callback(null, {
+                statusCode: '200',
+                licensePlate: event.licensePlate,
+                state: event.state,
+                thumbsDownCount: +thumbsDownCount + 1
+            });
         }
     });
 }
 
 function addNewLicensePlateToDatabase(event, callback) {
     dynamodb.putItem({
-        TableName: ThumbsDownTableName,
+        TableName: 'ThumbsDown',
         Item: {
             'licensePlate': { S: event.licensePlate },
             'state': { S: event.state },
@@ -65,6 +70,9 @@ function addNewLicensePlateToDatabase(event, callback) {
         } else {
             callback(null, {
                 statusCode: '200',
+                licensePlate: event.licensePlate,
+                state: event.state,
+                thumbsDownCount: 1
             });
         }
     });
